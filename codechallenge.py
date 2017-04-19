8c90178c6331f85b17e22fa5d35b3c3997dddf80
Venkateshwar Reddy Jambula <jvr605@gmail.com>
2017-04-19 08:01:05 +0530

    Update codechallenge.py

--- codechallenge.py
+++ codechallenge.py
@@ -1,3 +1,17 @@
 
 #Link to MSD code Challenge
 # http://marketing-ao.madstreetden.com/acton/media/25759/msd-coding-challenge
+import pandas as pd
+import time
+
+start = time.time()
+df = pd.read_csv("/Users/jvr605/Downloads/msd.log"," ",header=None )
+df[0]=df[0]+" "+df[1]
+del(df[1])
+df.columns = ['DATE', 'IPADDR','URL','STATUS']
+
+query=pd.read_csv("/Users/jvr605/Downloads/ip_query.log"," ",header=None )
+
+final_output=query.isin(df.IPADDR).astype(int)
+print final_output
+print time.time() - start
