d14884ad415047c9d109bf5284ccfa2e370a0deb
Venkateshwar Reddy Jambula <jvr605@gmail.com>
2017-04-19 19:23:09 +0530

    Update codechallenge.py

--- codechallenge.py
+++ codechallenge.py
@@ -3,15 +3,37 @@
 # http://marketing-ao.madstreetden.com/acton/media/25759/msd-coding-challenge
 import pandas as pd
 import time
+import sys
 
 start = time.time()
-df = pd.read_csv("/Users/jvr605/Downloads/msd.log"," ",header=None )
+
+#Reding the input files
+#log_file_name="msd.log"
+#query_file_name = "ip_query.log"
+if len(sys.argv) > 1:
+    print "there are", len(sys.argv)-1, "arguments:"
+    for arg in sys.argv[1:]:
+        print arg
+else:
+    print "there are no arguments!"
+    raise  ValueError('PLEASE PASS THE LOG FILE AND IPQUERY FILE AS INPUT TO SCRIPT') 
+    
+print sys.argv[1] ,sys.argv[2]
+log_file_name=sys.argv[1]
+query_file_name = sys.argv[2]
+
+
+df = pd.read_csv(log_file_name," ",header=None )
+query=pd.read_csv(query_file_name," ",header=None )
+
+#cleaning the data by the time stamp and Date 
+#Fromating the data helps to use Time series forcasting
 df[0]=df[0]+" "+df[1]
-del(df[1])
+del(df[1]) #Removing the unwanted time stamp data
 df.columns = ['DATE', 'IPADDR','URL','STATUS']
 
-query=pd.read_csv("/Users/jvr605/Downloads/ip_query.log"," ",header=None )
+
 
 final_output=query.isin(df.IPADDR).astype(int)
 print final_output
-print time.time() - start
+print "Execution Time of this script is  %f" %(time.time() - start)
