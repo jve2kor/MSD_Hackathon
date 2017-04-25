71df02e706f16e9ffed9166466a9994b0df37111
Venkateshwar Reddy Jambula <jvr605@gmail.com>
2017-04-25 11:25:35 +0530

    Update codechallenge.py

--- codechallenge.py
+++ codechallenge.py
@@ -1,52 +1,77 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+""" This code is used to read the log file into the memory  and convert into the data frame
+    Once the log file is loaded ,every item in the IPQuery file checked if exist and result is print onto the console"""
 
-#Link to MSD code Challenge
-# http://marketing-ao.madstreetden.com/acton/media/25759/msd-coding-challenge
-import pandas as pd
+
+#importing  python modules required for this script to perform operations
+
 import time
 import sys
 
-""" code to check the arguments passed   """
-if len(sys.argv) > 1:
-    print "there are", len(sys.argv)-1, "arguments:"
-    for arg in sys.argv[1:]:
-        print arg
-else:
-    print "There are no arguments!,Provide Log and IPquery file"
-    raise  ValueError('PLEASE PASS THE LOG FILE AND IPQUERY FILE AS INPUT TO SCRIPT') 
+
+
+start = time.time()#capturing time instance
+
+
+class IpQuery:
+    """Below methods contain the functionality to read file paths ,import log and query data 
+       and provide the result to the console """    
+    def __init__(self):
+        self.log_file_name= ""
+        self.query_file_name = ""
+        self.logset = {}
+        self.IPlist= []
+        
+    def Inputfiles(self):
+        """code to check the arguments passed  and throw an error  """
+        if len(sys.argv)!= 3:
+            raise  ValueError(""" PLEASE PASS THE BOTH LOG FILE AND IPQUERY FILE AS INPUT TO SCRIPT
+                              ex: python program.py log_file query_file """) 
+       # extracting file names from command line
+        self.log_file_name=sys.argv[1]
+        self.query_file_name = sys.argv[2]
+        
+    def read_logfile(self):
+        #Reading the log data
+        with open(self.log_file_name,'r') as f:
+            self.logset = {line.split(' ')[2] for line in f}
+
+
+    def read_Queryfile(self):
+        #Reading the query file into the  dataframe"""
+        with open(self.query_file_name,'r') as f:
+            self.IPlist = [line.rstrip('\n') for line in f]
+            
+
+    def CheckIpAdress(self):
+        #Ip address from query file ae checked against the log file """ 
+        dummy= self.logset.intersection(set(self.IPlist))
     
-#extracting file names from command line
-log_file_name=sys.argv[1]
-query_file_name = sys.argv[2]
+        for element in self.IPlist:
+            if element in dummy:
+                print "1"
+            else :
+                print "0"
+                
 
 
 
-#extracting file names from command line
-#log_file_name = "/Users/jvr605/Downloads/msd.log"
-#query_file_name = "/Users/jvr605/Downloads/ip_query.log"
+#Create an instance of the IpQuery file
+msd=IpQuery()
+#Extacting the input file information 
+msd.Inputfiles()
+#Importing the Ip informatin from the log files
+msd.read_logfile()
+#Importing the Ipquery information from the quer file
+msd.read_Queryfile()
+#Serching for the Ip in log file
+msd.CheckIpAdress()
 
-start = time.time()
 
-#Reading the content from the log file into dataframe log_df
-log_df = pd.read_csv(log_file_name," ",header=None ,names = ['DATE','TIME', 'IPADDR','URL','STATUS'],skip_blank_lines = True)
 
-#Reading the content from the IPquery file into the data frame query_df
-query_df = pd.read_csv(query_file_name," ",header=None,skip_blank_lines=True )
-
-#Cheking if the IP address exists in the log file
-Ipfound = query_df.isin(log_df.IPADDR).astype(int)
-
-#print Ipfound[0].values.astype(int)
-
-for items in Ipfound[0]:
-    print items
+            
     
- 
-    
-#for everritem in     query_df[0]:
-#    if everritem in log_df.IPADDR:
-#        print 1
-#    else:
- #       print 0
-    
-
+                
 print "Execution Time of this script is  %f" %(time.time() - start)
+                
